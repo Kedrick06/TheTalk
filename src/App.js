6a40557576a6bf6523e3541a9410971925466375
Kedrick06 <bfloblk@gmail.com>
2020-08-21 15:59:13 -0500

    commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Nav from './Components/Nav';
+
+function App() {
+  return (
+    <div className="container-app">
+      <Nav/>
+      <h2 className="App-header">New Interview</h2>
+      <h2 className="App-header2">Interview Directors</h2>
+      <div className="box-1"></div>
+      <div className="item item1"></div>
+      <div className="item item2"></div>
+      <div className="item item3"></div>
+      <div className="item item4"></div>
+      <div className="item item5"></div>
+      <div className="item item6"></div>
+      <div className="item item7"></div>
+      <div className="item item8"></div>
+      <h2 className="sub-header">MATT DAMON: "THESE TYPES OF MOVIES ARE GOING AWAY"</h2>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+class Nav extends Component {
+    render() {
+
+    return(
+        <div className="nav-bar">
+            <nav class="nav-list">
+                <h1>TheTalk</h1>
+            </nav>
+        </div>
+        
+    )
+
+}
+}
+
+
+
+export default Nav;
