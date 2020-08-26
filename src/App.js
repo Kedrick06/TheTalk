dfda697791c8187d53eaca6b7f82a8164942e396
Kedrick06 <bfloblk@gmail.com>
2020-08-26 18:03:53 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ function App() {
   return (
     <div className="container-app">
       <Nav/>
-      <div className="box-1"></div>
+      <div className="box-1">
       <h2 className="App-header">New Interview</h2>
       <h2 className="App-header2">Interview Directors</h2>
-      <div className="item item1">
-        <img src="https://the-talks.com/wp-content/uploads/2019/12/Matt-Damon-01.jpg" ></img>
+      <div className="item-one">
+        <img src="https://the-talks.com/wp-content/uploads/2019/12/Matt-Damon-01.jpg"></img>
+        <h2 className="sub-header">MATT DAMON: "THESE TYPES OF MOVIES ARE GOING AWAY"</h2>
       </div>
-      <div className="item item2"></div>
-      <div className="item item3"></div>
-      <div className="item item4"></div>
-      <div className="item item5"></div>
-      <div className="item item6"></div>
-      <div className="item item7"></div>
-      <div className="item item8"></div>
-      <h2 className="sub-header">MATT DAMON: "THESE TYPES OF MOVIES ARE GOING AWAY"</h2>
-   </div>
+      <div className="item-two"></div>
+      <div className="item-three"></div>
+      <div className="item-four"></div>
+      <div className="item-five"></div>
+      <div className="item-six"></div>
+      <div className="item-seven"></div>
+      <div className="item-eight"></div>
+      <footer class="orange">Footer</footer>
+    </div>
+  </div>
   );
 }
 
