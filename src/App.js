79334ca0a8dedea38da1e015fb2667583da96419
Kedrick06 <bfloblk@gmail.com>
2020-08-28 15:40:20 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -8,22 +8,24 @@ function App() {
     <div className='blue'>
       <Nav/>
     </div>
-    <div className='red'>
+    <div className='matt-damon'>
       <img src="https://the-talks.com/wp-content/uploads/2019/12/Matt-Damon-01.jpg"></img>
+      <div>
+      <h2 className='header-class'>MATT DAMON: “THESE TYPES OF MOVIES ARE GOING AWAY”</h2></div>
     </div>
-    <div className='green'>
+    <div className='side-section'>
         <div className='greenchild'>
-            <div className='one'>1</div>
-            <div className='two'>2</div>
-            <div className='three'>3</div>
-            <div className='four'>4</div>
-            <div className='five'>5</div>
-            <div className='six'>6</div>
-            <div className='seven'>7</div>
-            <div className='eight'>8</div>
+            <div className='architecture-class'>ARCHITECTURE</div>
+            
+            <div className='art-class'>ART</div>
+            <div className='fashion-class'>FASHION</div>
+            <div className='film-class'>FILM</div>
+            <div className='food-class'>FOOD</div>
+            <div className='literature-class'>LITERATURE</div>
+            <div className='music-class'>MUSIC</div>
+            <div className='sports-class'>SPORTS</div>
         </div>
     </div>
-    <footer className='footer-class'>Footer</footer>
 </div>
   );
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,6 +6,9 @@ class Nav extends Component {
 
     return(
         <div className="blue">
+            <div>
+           <img src='https://the-talks.com/wp-content/themes/thetalks/img/the-talks-logo@2x.png'></img>
+            </div>
             <nav class="nav-list">
                 <h1>The Talks |  Interview Directory  |  World Guide</h1>
             </nav>
